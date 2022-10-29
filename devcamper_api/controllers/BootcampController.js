0d61cefbbe3ec09b365ec43667dad791ed71f472
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-10-30 02:00:16 +0500

    routes with controller with express

---
+++ devcamper_api/controllers/BootcampController.js
@@ -0,0 +1,5 @@
+exports.getAllBootcamp = async (req, res, next) => {};
+exports.getSingleBootcamp = async (req, res, next) => {};
+exports.createBootcamp = async (req, res, next) => {};
+exports.updateBootcamp = async (req, res, next) => {};
+exports.deleteBootcamp = async (req, res, next) => {};
