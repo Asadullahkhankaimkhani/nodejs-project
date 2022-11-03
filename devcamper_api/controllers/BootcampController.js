4855ff5548e68f5742fe9371c611febb03e0dff4
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-11-03 23:58:42 +0500

    Fetch Bootcamps

--- devcamper_api/controllers/BootcampController.js
+++ devcamper_api/controllers/BootcampController.js
@@ -1,7 +1,36 @@
 const Bootcamp = require("../models/BootcampModel");
 
-exports.getAllBootcamp = async (req, res, next) => {};
-exports.getSingleBootcamp = async (req, res, next) => {};
+exports.getAllBootcamp = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({
+      status: true,
+      bootcamps,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: false,
+    });
+  }
+};
+exports.getSingleBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({
+        status: false,
+      });
+    }
+    return res.status(200).json({
+      status: true,
+      bootcamp,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      status: false,
+    });
+  }
+};
 
 exports.createBootcamp = async (req, res, next) => {
   try {
