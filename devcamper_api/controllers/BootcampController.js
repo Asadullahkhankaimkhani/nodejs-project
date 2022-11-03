43a98577cc5ee7780305efd9547cbbb1e678ffa7
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-11-03 23:50:30 +0500

    create bootcamp

--- devcamper_api/controllers/BootcampController.js
+++ devcamper_api/controllers/BootcampController.js
@@ -1,5 +1,18 @@
+const Bootcamp = require("../models/BootcampModel");
+
 exports.getAllBootcamp = async (req, res, next) => {};
 exports.getSingleBootcamp = async (req, res, next) => {};
-exports.createBootcamp = async (req, res, next) => {};
+
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json(bootcamp);
+  } catch (err) {
+    return res.status(400).json({
+      status: false,
+    });
+  }
+};
+
 exports.updateBootcamp = async (req, res, next) => {};
 exports.deleteBootcamp = async (req, res, next) => {};
