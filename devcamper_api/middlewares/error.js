88b1d1c2be8e4c38d9e5a27590d98d67acf61c35
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-11-06 00:40:19 +0500

    Mongoose Error handler 2

--- devcamper_api/middlewares/error.js
+++ devcamper_api/middlewares/error.js
@@ -1,12 +1,24 @@
-const ErrorReposnse = require("../utils/errorResponse");
+const ErrorResponse = require("../utils/errorResponse");
 const errorHandler = (err, req, res, next) => {
   // console.log(err.stack.red);
   let error = { ...err };
   error.message = err.message;
-
+  // Mongoose bad ObjectId
   if (err.name === "CastError") {
     const message = `Resource not found with id of ${err.value}`;
-    error = new ErrorReposnse(message, 404);
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation Error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
