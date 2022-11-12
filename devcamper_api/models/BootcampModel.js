2ee6ef481ab26fa7a671a5b139350018ea108a7c
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-11-12 21:29:54 +0500

    GeoJson location & geocoder hook - MapQuestAPI

--- devcamper_api/models/BootcampModel.js
+++ devcamper_api/models/BootcampModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 const bootcampSchema = new Schema(
   {
@@ -103,6 +104,23 @@ const bootcampSchema = new Schema(
 
 bootcampSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lowercase: true });
+  next();
+});
+
+bootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+  this.address = undefined;
+
   next();
 });
 
