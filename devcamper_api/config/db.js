cb93b7655812f6bc3261105aa3da74051f7ffa8c
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-11-17 12:49:40 +0500

    Colors

--- devcamper_api/config/db.js
+++ devcamper_api/config/db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+const colors = require("colors");
 
 const dbConnect = (async function () {
   try {
     await mongoose.connect(process.env.DB_STRING);
-    console.log("########## Database Connected ##########");
+    console.log(
+      colors.bgYellow.yellow("########## Database Connected ##########")
+    );
   } catch (error) {
     console.log(" Database Error ", error);
   }
--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -4,6 +4,7 @@ dotenv.config({ path: "./config/config.env" });
 const morgan = require("morgan");
 const dbconnect = require("./config/db");
 const errorHandler = require("./middlewares/error");
+const colors = require("colors");
 
 // Modules
 const bootcamp = require("./routes/BootcampRoute");
@@ -25,7 +26,11 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    colors.bgWhite.green(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+  )
 );
 
 process.on("unhandledRejection", (err, promise) => {
