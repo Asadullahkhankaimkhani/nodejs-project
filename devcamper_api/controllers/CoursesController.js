46b2e1a8b953585afa7ba61bcfa765586efe2850
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-11-17 15:23:24 +0500

    Update and Delete Course

--- devcamper_api/controllers/CoursesController.js
+++ devcamper_api/controllers/CoursesController.js
@@ -62,3 +62,40 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     data: course,
   });
 });
+
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with this id of ${req.params.bootcampId}`),
+      404
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with this id of ${req.params.bootcampId}`),
+      404
+    );
+  }
+  await course.remove();
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
--- devcamper_api/routes/CoursesRoute.js
+++ devcamper_api/routes/CoursesRoute.js
@@ -4,9 +4,11 @@ const {
   getCourses,
   getCourse,
   addCourse,
+  updateCourse,
+  deleteCourse,
 } = require("../controllers/CoursesController");
 
 router.route("/").get(getCourses).post(addCourse);
-router.route("/:id").get(getCourse);
+router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
