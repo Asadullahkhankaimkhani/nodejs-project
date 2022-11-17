90dba62f740eb522c4b51be636956e94acd3f4e0
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-11-17 17:21:23 +0500

    Aggregate average of bootcamp courses

--- devcamper_api/seeder.js
+++ devcamper_api/seeder.js
@@ -30,7 +30,7 @@ const course = JSON.parse(
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamp);
-    await Course.create(course);
+    // await Course.create(course);
     console.log("Data Imported...");
     process.exit();
   } catch (error) {
