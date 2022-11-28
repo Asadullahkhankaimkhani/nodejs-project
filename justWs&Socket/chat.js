e732a4ef400ef9d08214cd3582f7a5be2301a5aa
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-11-28 16:04:23 +0500

    Finishing the chat app

--- justWs&Socket/chat.js
+++ justWs&Socket/chat.js
@@ -6,11 +6,19 @@ app.use(express.static(__dirname + "/public"));
 
 const expressServer = app.listen(9000);
 
-const io = socketio(expressServer);
+const io = socketio(expressServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
   socket.emit("messageFromServer", { data: "Welcome to the socket io server" });
   socket.on("messageToServer", (dataFromClient) => {
     console.log(dataFromClient);
+  });
+  socket.on("newMessageToServer", (msg) => {
+    io.emit("messageToClient", { text: msg.text });
   });
 });
