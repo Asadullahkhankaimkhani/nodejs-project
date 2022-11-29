a6446d49f7a0e114ff5cc8ce65bed3fb7573dff1
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-11-29 17:56:01 +0500

    Cheat Sheet Namespace

--- justWs&Socket/2 - Socket201/public/index.js
+++ justWs&Socket/2 - Socket201/public/index.js
@@ -25,3 +25,31 @@ document.querySelector("#message-form").addEventListener("submit", (event) => {
 socket.on("messageToClient", (msg) => {
   document.querySelector("#message").innerHTML += ` <li>${msg.text}</li>`;
 });
+
+// Send an event from the server to this socket only:
+
+// socket.emit()
+// socket.send()
+
+// Send an event from a socket to a room:
+
+// NOTE: remember, this will not go to the sending socket
+
+// socket.to(roomName).emit()
+// socket.in(roomName).emit()
+
+// Because each socket has it's own room, named by it's socket.id, a socket can send a message to another socket:
+
+// socket.to(anotherSocketId).emit('hey');
+// socket.in(anotherSocketId).emit('hey');
+
+// A namespace can send a message to any room:
+
+// io.of(aNamespace).to(roomName).emit()
+// io.of(aNamespace).in(roomName).emit()
+
+// A namespace can send a message to the entire namespace
+
+// io.emit()
+// io.of('/').emit()
+// io.of('/admin').emit()
