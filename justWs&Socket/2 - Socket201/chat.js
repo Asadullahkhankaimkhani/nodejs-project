58d597869e2d2b15746b52eed23a46459a1efb05
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-11-29 13:44:46 +0500

    Restructure Folder

---
+++ justWs&Socket/2 - Socket201/chat.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const socketio = require("socket.io");
+
+app.use(express.static(__dirname + "/public"));
+
+const expressServer = app.listen(9000);
+
+const io = socketio(expressServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.emit("messageFromServer", { data: "Welcome to the socket io server" });
+  socket.on("messageToServer", (dataFromClient) => {
+    console.log(dataFromClient);
+  });
+  socket.on("newMessageToServer", (msg) => {
+    io.emit("messageToClient", { text: msg.text });
+  });
+});
