d657734a31b81f888027ea1ecd823944ce1951aa
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-11-30 13:52:06 +0500

    Step 1 - 3 Complete

--- Socket.io/3 - Slack/public/index.js
+++ Socket.io/3 - Slack/public/index.js
@@ -1,50 +1,27 @@
 const socket = io("http://localhost:9000");
-const socket2 = io("http://localhost:9000/admin");
+
+socket.on("nsList", (nsData) => {
+  let namespaceDiv = document.querySelector(".namespaces");
+  namespaceDiv.innerHTML = "";
+  nsData.forEach((ns) => {
+    namespaceDiv.innerHTML += `<div class="namespace" ns="${ns.endpoint}"><img src="${ns.img}" /></div>`;
+  });
+
+  // Add a click listener
+  Array.from(document.getElementsByClassName("namespace")).forEach((elem) => {
+    // console.log(elem.getAttribute("ns"));
+
+    elem.addEventListener("click", (e) => {
+      const nsEndpoint = elem.getAttribute("ns");
+    });
+  });
+});
 
 socket.on("messageFromServer", (dataFromServer) => {
   console.log(dataFromServer);
   socket.emit("messageToServer", { data: "This is from the client" });
 });
 
-socket2.on("welcome", (datafromeServer) => {
-  console.log(datafromeServer);
-});
-
 socket.on("joined", (msg) => {
   console.log(msg);
 });
-
-// document.querySelector("#message-form").addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const newMessage = document.querySelector("#user-message").value;
-//   console.log(newMessage);
-//   socket.emit("newMessageToServer", { text: newMessage });
-// });
-
-// Send an event from the server to this socket only:
-
-// socket.emit()
-// socket.send()
-
-// Send an event from a socket to a room:
-
-// NOTE: remember, this will not go to the sending socket
-
-// socket.to(roomName).emit()
-// socket.in(roomName).emit()
-
-// Because each socket has it's own room, named by it's socket.id, a socket can send a message to another socket:
-
-// socket.to(anotherSocketId).emit('hey');
-// socket.in(anotherSocketId).emit('hey');
-
-// A namespace can send a message to any room:
-
-// io.of(aNamespace).to(roomName).emit()
-// io.of(aNamespace).in(roomName).emit()
-
-// A namespace can send a message to the entire namespace
-
-// io.emit()
-// io.of('/').emit()
-// io.of('/admin').emit()
--- Socket.io/3 - Slack/server.js
+++ Socket.io/3 - Slack/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const socketio = require("socket.io");
 const namespaces = require("./data/namespaces");
-console.log(namespaces);
 
 app.use(express.static(__dirname + "/public"));
 
@@ -16,25 +15,20 @@ const io = socketio(expressServer, {
 });
 
 io.on("connection", (socket) => {
-  socket.emit("messageFromServer", { data: "Welcome to the socket io server" });
-  socket.on("messageToServer", (dataFromClient) => {
-    console.log(dataFromClient);
+  let nsData = namespaces.map((ns) => {
+    return {
+      img: ns.img,
+      endpoint: ns.endpoint,
+    };
   });
-
-  socket.join("level1");
-  socket
-    .to("level1")
-    .emit("joined", `${socket.id} say i joined the level 1 room`);
-
-  // TODO:  Note
-  // The server can still communicate across namespaces
-  // but on the clientInformation, the socket needs to in that namespaces
-  // in order to get the events
-  // setTimeout(() => {
-  //   io.of("/admin").emit("welcome", "welcome from main channel admin");
-  // }, 2000);
+  // send the nsData back  to the client. we need to use socket, not io. because we want it to
+  // go the just this client
+  socket.emit("nsList", nsData);
 });
-io.of("/admin").on("connection", (socket) => {
-  // socket2.emit("welcome", "welcome message form admin channel");
-  io.of("/admin").emit("welcome", "Welcome to the admin channel 1.0");
+
+// loop through each namespaces and listen for a connections
+namespaces.forEach((namespace) => {
+  io.of(namespace.endpoint).on("connection", (socket) => {
+    console.log(`${socket.id} has join ${namespace.endpoint}`);
+  });
 });
