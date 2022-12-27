ac79d51b5a202b8c27c0736a7a4496550da00f6c
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-12-28 00:45:33 +0500

    Basic Validation

--- task-manager-api/Model/task.model.js
+++ task-manager-api/Model/task.model.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    trim: true,
+    required: [true, "must provide name"],
+    maxlength: [20, "name can not be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", taskSchema);
