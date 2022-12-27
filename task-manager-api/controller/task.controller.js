0414775985795eaffdf13d4fb96f50f0eaf2a008
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-12-28 01:17:10 +0500

    Try/catch Block

--- task-manager-api/controller/task.controller.js
+++ task-manager-api/controller/task.controller.js
@@ -1,8 +1,12 @@
 const Task = require("../Model/task.model");
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json(task);
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
 const getAllTask = (req, res) => {
