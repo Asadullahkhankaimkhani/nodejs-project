29f27101300c50fa14824b75a730949ef9d43537
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2023-01-28 00:13:09 +0500

    Basic Express Server

--- Store-Api/server.js
+++ Store-Api/server.js
@@ -1,1 +1,32 @@
-console.log("init");
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+// imports
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1>");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
