935c23287d7d9e9532dd706824f1cf0e79fe9ccd
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2023-02-12 14:30:15 +0500

    update server

--- task-manager-api/server.js
+++ task-manager-api/server.js
@@ -2,16 +2,21 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 require("./config/db");
+const cors = require("cors");
+
 // import routes
 const task = require("./routes/task.route");
+const morgan = require("morgan");
 
 // Middleware
 
 app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
 // routes
 app.get("/", (req, res) => {
-  res.send("Task Manager");
+	res.send("Task Manager");
 });
 
 app.use("/api/v1", task);
