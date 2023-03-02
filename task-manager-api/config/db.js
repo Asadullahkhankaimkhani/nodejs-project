888f06925459d8ad0380f74f90554126e7706d1b
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2023-03-02 23:52:53 +0500

    Update db.js

--- task-manager-api/config/db.js
+++ task-manager-api/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
+// Database Connection
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("Database Connected!"))
-  .catch((err) => console.log(err));
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log("Database Connected!"))
+	.catch((err) => console.log(err));
